40d8a549d4af49150bbb741a48e45ca417e27427
unknown <boriintorr@gmail.com>
2020-05-31 02:16:30 +0300

    fixed clock order change not being saved

--- src/App.js
+++ src/App.js
@@ -86,6 +86,7 @@ const App = () => {
 
         newClocks.splice(newIndex, 0, oldClockObj);
         setClocks(newClocks);
+        localStorage.setItem('clocks', JSON.stringify(newClocks));
     };
 
     let clockList = clocks.map((clock) => {
